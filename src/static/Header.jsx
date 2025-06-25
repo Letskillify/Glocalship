b576b6104d1a003bb9044bcf2d2f3d2097dab60d
Arpit562 <arpitkumarprajapat706@gmail.com>
2025-06-25 13:08:21 +0530

    Updated header

--- src/static/Header.jsx
+++ src/static/Header.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase/firebaseConfig';
 import { signOut } from 'firebase/auth';
+
 
 function Header() {
   const user = auth.currentUser;
@@ -16,11 +17,12 @@ function Header() {
   };
 
   return (
-    <header className="sticky-top shadow-sm bg-white ">
+    <header className='sticky-top shadow-sm' style={{ backgroundColor: 'var(--primary)' }}>
       <nav className=" container navbar navbar-expand-md navbar-light container-fluid px-2">
         <Link className="navbar-brand d-flex align-items-center" to="/">
-          <img src="/assets/img/logo.webp" alt="Logo" height="40" className="me-2" />
-          <h5 className="mb-0 fw-bold">Scout</h5>
+          <h5 className="mb-0 fw-bold " style={
+            { color: 'var(--secondary)' }
+          }>Glocalship</h5>
         </Link>
 
         <button
@@ -34,32 +36,30 @@ function Header() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className={`collapse navbar-collapse ${!isNavCollapsed ? 'show' : ''}`} id="navbarNav">
-          <div></div>
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-3 text-center">
+        <div className={`collapse navbar-collapse ${!isNavCollapsed ? 'show' : ''}`} id="navbarNav" >
+
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-lg-3 text-center" >
             <li className="nav-item">
-              <a className="nav-link" href="#hero">Home</a>
+              <Link className="nav-link" to="">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#about">About</a>
+              <Link className="nav-link" to="">About</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#services">Services</a>
+              <Link className="nav-link" to="">Services</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/parcel">Book Parcel</Link>
+              <Link className="nav-link" to="">Book Parcel</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/track">Track Parcel</Link>
+              <Link className="nav-link" to="">Track Parcel</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/bookings">My Bookings</Link>
-            </li>
-            <li className="nav-item">
-             
-              <button onClick={handleLogout} className="btn btn-danger btn-sm mt-2 mt-lg-2">
-                Logout
-              </button>
+              <Link className="nav-link" to="">
+                <span><i class="fa-solid fa-user "></i>
+                </span>
+              </Link>
+
             </li>
           </ul>
         </div>
